f205f3f69926cb618f97a7ad7bed811cbd95bdf8
HubertSienicki <businesssienicki@gmail.com>
2022-03-24 10:03:33 +0000

    Added a config parser

---
+++ DatabaseDriver/config.py
@@ -0,0 +1,21 @@
+from configparser import ConfigParser
+
+def config(filename='database.ini', section='postgresql'):
+        
+    #Creates a parser
+    parser = ConfigParser()
+    #Read config file
+    parser.read(filename)
+
+    db = {}
+
+    if parser.has_section(section):
+         params = parser.items(section)
+
+        for param in params:
+            db[param[0]] = params[1]
+
+    else:
+        raise Exception('Section {0} does not exist in the {1} file'.format(section, filename))
+
+    return db
